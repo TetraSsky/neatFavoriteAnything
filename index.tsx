ebc9fbb4a9106afcf56d85fcde883a1106021ac8
Davri <davri@disroot.org>
2026-03-24 15:56:30 +0100

    Use unique find for embed patches

--- index.tsx
+++ index.tsx
@@ -29,7 +29,7 @@ export default definePlugin({
     patches: [
         // EMBEDS
         {
-            find: "#{intl::SUPPRESS_ALL_EMBEDS}",
+            find: "this.renderInlineMediaEmbed",
             replacement: [
                 {
                     // Wrap the embed component's render method in a custom context to avoid having to drill props
